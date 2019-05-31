8549e596d41d02d00e33c9d1aa9c434932febff3
TopsCode <kailash.solanki@tops-int.com>
2019-05-31 15:33:11 +0530

    Add files via upload

---
+++ Module-2/2.3 OOPS/2.11 Wrapper Class/2.11.1 WrapperClassDemo.java
@@ -0,0 +1,18 @@
+package com.fundamentals;
+
+public class WrapperClassDemo {
+
+	public static void main(String[] args) {
+	String n1,n2;
+	n1="100";
+	n2="300";
+int sum;
+   int x=Integer.parseInt(n1);
+   int y=Integer.parseInt(n2);
+   sum=x+y;
+   
+   System.out.println("Sum:"+sum);
+	
+	}
+
+}
