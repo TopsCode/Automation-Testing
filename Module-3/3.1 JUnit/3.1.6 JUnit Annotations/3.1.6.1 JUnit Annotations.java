c7266e76da5348306a8f25b221a59a3ad751fe75
Nivedita <shivaresnivedita@gmail.com>
2019-06-15 02:07:37 +0530

    Create 3.1.6.1 JUnit Annotations.java

---
+++ Module-3/3.1 JUnit/3.1.6 JUnit Annotations/3.1.6.1 JUnit Annotations.java
@@ -0,0 +1,57 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestJunit {
+  // execute before class
+  @BeforeClass
+  public static void beforeClass() {
+    System.out.println("in before class");
+  }
+
+  // execute after class
+  @AfterClass
+  public static void afterClass() {
+    System.out.println("in after class");
+  }
+
+  // execute before test
+  @Before
+  public void before() {
+    System.out.println("in before");
+  }
+
+  // execute after test
+  @After
+  public void after() {
+    System.out.println("in after");
+  }
+
+  // test case
+  @Test
+  public void test() {
+    System.out.println("in test");
+  }
+
+  // test case ignore and will not execute
+  @Ignore
+  public void ignoreTest() {
+    System.out.println("in ignore test");
+  }
+}
+
+public class Main {
+   public static void main(String[] args) {
+      Result result = JUnitCore.runClasses(TestJunit.class);
+      for (Failure failure : result.getFailures()) {
+         System.out.println(failure.toString());
+      }
+      System.out.println(result.wasSuccessful());
+   }
+}
