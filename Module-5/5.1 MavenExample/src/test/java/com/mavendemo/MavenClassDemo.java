95ef97e23c48dd08ef335d098ccadc66b6b74cc6
Nivedita <shivaresnivedita@gmail.com>
2019-06-20 23:31:56 +0530

    m

---
+++ Module-5/5.1 MavenExample/src/test/java/com/mavendemo/MavenClassDemo.java
@@ -0,0 +1,29 @@
+package com.mavendemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MavenClassDemo 
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\tops\\Java_ST_Automation2018\\selenium\\selenium\\driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+			driver=new ChromeDriver(); 
+			driver.get("http://automationpractice.com/index.php");
+			Thread.sleep(2000);
+			driver.findElement(By.linkText("Sign in")).click();
+			Thread.sleep(1000);
+			driver.findElement(By.name("email")).sendKeys("rahul.sanghavi.mca@gmail.com");
+			Thread.sleep(1000);
+			driver.findElement(By.name("passwd")).sendKeys("rahul12345");
+			Thread.sleep(1000);
+			driver.findElement(By.name("SubmitLogin")).click();
+			Thread.sleep(3000);
+			driver.findElement(By.linkText("Sign out")).click();
+			Thread.sleep(1000);
+			driver.close();
+	
+	}
+}
